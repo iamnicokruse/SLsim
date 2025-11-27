b6cc7f778e390ee05df0d20ca04d6421c75828b7
Nico Kruse <nicokr1607@icloud.com>
2025-11-27 14:53:00 +0100

    configurations in master script

--- SLsim_master.R
+++ SLsim_master.R
@@ -3,12 +3,14 @@
 ###############################################################################
 
 # packages
-library("caret")
-library("caretEnsemble")
-library("glmnet")
-library("ranger") 
-library("gbm")
-library("tidyverse")
+library(caret)
+library(caretEnsemble)
+library(glmnet)
+library(ranger)
+library(gbm)
+library(tidyverse)
+library(future)
+library(future.apply)
 
 # parameters
 source("MLsim-main/utils/setParameters.R")
@@ -25,11 +27,17 @@ source("SLfunction")
 source("Model_evaluation.R")
 
 # generate folder for log files (if needed!)
+logFolder = "log"
+createFolder(logFolder) # we do not save data but maybe we need to extract log 
+                        # files in case of errors
 
 # set numbers of Cores to use in parallel computing
+nCoresSampling <- 3 # needs to be changed when cluster is used
+                    # computer used to build code only allows using 3 Cores
 
 # generate grid with all combinations of simulation conditions
-
+gridInter <- expand.grid(N = setParam$dgp$N,
+                         reliability = setParam$dgp$reliability)
 
 # add seeds to grid
 
