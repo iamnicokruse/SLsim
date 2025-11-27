9fa5ab2348a0ca2551b270937a7e1da08628b206
Nico Kruse <nicokr1607@icloud.com>
2025-11-27 13:44:15 +0100

    initialize master script to be used on cluster

---
+++ SLsim_master.R
@@ -0,0 +1,34 @@
+################################################################################
+#_______________________Super Learner Simulation Project_______________________#
+################################################################################
+
+# packages
+
+# parameters and specific functions
+
+# simulation functions
+# analysis functions
+
+# generate folder for log files (if needed!)
+
+# set numbers of Cores to use in parallel computing
+
+# generate grid with all combinations of simulation conditions
+
+
+# add seeds to grid
+
+
+# add dgp type column to grid
+
+# add other dgps and seeds
+
+# check uniqueness of set seeds
+
+# sample data im parallel
+
+# simulate and return data
+
+# fit super learner to data and save results
+
+
