3c9674941a8e28038846fded5ce0f9b9063d5c89
Nico Kruse <nicokr1607@icloud.com>
2025-12-04 09:29:44 +0100

    adjusted paths so they work without prior setwd()

--- MLsim-main/utils/setParameters.R
+++ MLsim-main/utils/setParameters.R
@@ -85,11 +85,11 @@ setParam$fit$optimTol <- 1e-5
 setParam$fit$optimBetaTol <- 1e-5
 
 # read in coefficients as results from bruteForceB.R and bruteForceNonLinearB.R
-bruteForceB_inter <- read.table("utils/bruteForceBcoeff_inter.csv", header = T, sep = ",")
+bruteForceB_inter <- read.table("MLsim-main/utils/bruteForceBcoeff_inter.csv", header = T, sep = ",")
 # bruteForceB_nl <- read.table("utils/bruteForceBcoeff_nonlinear.csv", header = T, sep = ",")
-bruteForceB_nl3 <- read.table("utils/bruteForceBcoeff_nonlinear3.csv", header = T, sep = ",")
+bruteForceB_nl3 <- read.table("MLsim-main/utils/bruteForceBcoeff_nonlinear3.csv", header = T, sep = ",")
 # bruteForceB_nl <- read.table("utils/bruteForceBcoeff_nonlinear_plus.csv", header = T, sep = ",")
-bruteForceB_pwl <- read.table("utils/bruteForceBcoeff_piecewise.csv", header = T, sep = ",")
+bruteForceB_pwl <- read.table("MLsim-main/utils/bruteForceBcoeff_piecewise.csv", header = T, sep = ",")
 
 # reorganize coefficients
 # # lin vs. interaction
