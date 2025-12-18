732ca5ea15897e576da2ec27ba706d752407a9d1
Nico Kruse <nicokr1607@icloud.com>
2025-12-18 17:31:56 +0100

    started new sl function

---
+++ fitSuperLearner.R
@@ -0,0 +1,146 @@
+# Function to train Super Learner to data
+
+# it needs to adapt list of sample[...]Data (dataList) to runSl function 
+# X_int can be used as predictor matrix
+# each of yMat 9 columns is used once as criterion for runSL
+
+dataList <- out[[1]]
+
+runSL <- function(dataList){
+  Xint = as.data.frame(dataList$X_int[, !grepl(":", colnames(dataList$X_int))])
+  yVec = colnames(dataList$yMat)
+
+  for(y in seq_along(yVec)){
+    krit <- dataList$yMat[, y]
+    data <- data.frame(Xint, krit)
+    
+    preds <- names(data[!(names(data) %in% c("krit"))]) # predictor character string
+    mod <- as.formula(paste("krit ~ ", paste(preds, collapse = "+"))) # additive predictor combination
+    
+    trainCtrl <- trainControl(method = "cv",       # specification of tuning in inner cv for baselearner
+                              number = 10,
+                              savePredictions = "final", # saves predictions for optimal tuning parameters
+                              allowParallel = F) # must be set to FALSE, as we parallelize the outer resampling
+    
+    models <- caretList(mod,
+                        data = train_data,
+                        trControl = trainCtrl,
+                        metric = "MAE",
+                        methodList = setParam$modfit$baselearner
+    )
+    
+    ensemCtrl <- trainControl(method = "cv",       # specification of tuning in inner cv for metalearner
+                              number = 10,
+                              savePredictions = "final", # saves predictions for optimal tuning parameters
+                              allowParallel = F) # must be set to FALSE, as we parallelize the outer resampling
+    slList <- list()
+    for(s in seq_along(setParam$modfit$superlearner)) {
+      metalearner = setParam$modfit$superlearner[s]
+    
+      if(metalearner == "ranger"){
+      ensemble <- caretStack(models,
+                             method = metalearner,
+                             metric = "MAE", 
+                             trControl = ensemCtrl,
+                             tuneLength = 25,
+                             importance = "impurity"         # this argument is needed for ranger as meta model
+      )                                                      # Impurity: reduction of node impurity by feature (faster)
+      # Permutation: permutated feature's impact on prediction
+      # "How much worse" (more fair)
+      } else {
+      ensemble <- caretStack(models,
+                             method = metalearner,           # if changed, also change weight-extraction and ensemble recoding in train_perf
+                             metric = "MAE", 
+                             trControl = ensemCtrl,
+                             tuneLength = 25
+      )}
+      # saving hyperparameters in a list
+      hyperparameters <- list(glmnet = models$glmnet$bestTune,
+                              rpart = models$rpart$bestTune,
+                              gbm = models$gbm$bestTune,
+                              rf = models$rf$bestTune,
+                              ensemble = ensemble$ens_model$bestTune)
+      
+      # saving weight of metalearner
+      if(metalearner == "glm") {
+        weights_metamodel <- ensemble$ens_model$finalModel$coefficients
+      } else if(metalearner == "glmnet") {
+        weights_metamodel <- as.matrix(coef(ensemble$ens_model$finalModel,            
+                                            s = ensemble$ens_model$bestTune$lambda))
+      } else if(metalearner == "ranger") {
+        weights_metamodel <- ensemble$ens_model$finalModel$variable.importance
+      } else{paste0("No specification of weight extraction for this metalearner")}   
+      
+      
+      if(metalearner == "glm") {
+        train_perf = rbind(glmnet_train = getTrainPerf(models$glmnet),
+                           rpart_train = getTrainPerf(models$rpart),
+                           gbm_train = getTrainPerf(models$gbm),
+                           rf_train = getTrainPerf(models$ranger)) %>%
+          mutate(method = recode(method, ranger = "rf")) %>%
+          rbind(ensemble_train = getTrainPerf(ensemble$ens_model)) %>%
+          mutate(method = recode(method, glm = "ensemble")) %>%  
+          rename(methods = method)
+      } else if(metalearner == "glmnet") {
+        train_perf = rbind(glmnet_train = getTrainPerf(models$glmnet),
+                           rpart_train = getTrainPerf(models$rpart),
+                           gbm_train = getTrainPerf(models$gbm),
+                           rf_train = getTrainPerf(models$ranger)) %>%
+          mutate(method = recode(method, ranger = "rf")) %>%
+          mutate(method = recode(method, glmnet = "enetglm")) %>%       # temporarily renamed to not be relabeled "ensemble"        
+          rbind(ensemble_train = getTrainPerf(ensemble$ens_model)) %>%  # as metalearner = "glmnet" gets relabeled  
+          mutate(method = recode(method, glmnet = "ensemble")) %>%        
+          rename(methods = method) %>%                                  
+          mutate(methods = recode(methods, enetglm = "glmnet"))         # baselearner glmnet gets orignal name back
+      } else if(metalearner == "ranger") {
+        train_perf = rbind(glmnet_train = getTrainPerf(models$glmnet),
+                           rpart_train = getTrainPerf(models$rpart),
+                           gbm_train = getTrainPerf(models$gbm),
+                           rf_train = getTrainPerf(models$ranger)) %>%
+          mutate(method = recode(method, ranger = "rf")) %>%
+          rbind(ensemble_train = getTrainPerf(ensemble$ens_model)) %>%    
+          mutate(method = recode(method, ranger = "ensemble")) %>%        
+          rename(methods = method)                          
+      }
+      
+      train_perf = cbind(train_perf, iter = rep(i, length(baselearner)+1))
+      
+      # evaluate final model using held out "test_data" and comparing performances
+      final_model <- ensemble
+      
+      # next step adds predictions based on metalearner to test_data
+      test_data <- cbind(test_data,
+                         pred = predict(final_model, test_data, na.action = na.pass))
+      
+      # adds predicitons based on baselearners to test_data
+      test_data$glmnet_pred <- predict(models$glmnet, test_data)
+      test_data$rpart_pred  <- predict(models$rpart, test_data)
+      test_data$gbm_pred  <- predict(models$gbm, test_data)
+      test_data$rf_pred  <- predict(models$ranger, test_data)
+      
+      
+      test_perf = data.frame(rbind(glmnet_test = postResample(pred =  test_data$glmnet_pred, obs = test_data$krit),
+                                   rpart_test = postResample(pred = test_data$rpart_pred, obs = test_data$krit),
+                                   gbm_test = postResample(pred = test_data$gbm_pred, obs = test_data$krit),
+                                   rf_test = postResample(pred = test_data$rf_pred, obs = test_data$krit),
+                                   ensemble_test = postResample(pred = test_data$pred, obs = test_data$krit))) %>%
+        rename(TestRMSE = RMSE) %>%
+        rename(TestRsquared = Rsquared) %>%
+        rename(TestMAE = MAE)
+      
+      models <- c("glmnet", "rpart", "gbm", "ranger", "ensemble")
+      test_perf = cbind(test_perf, models, iter = rep(i, length(baselearner)+1))
+      
+      train_perf$dataset <- basename(file)  # add sample name to train-output
+      test_perf$dataset  <- basename(file)  # add sample name to test-output
+      
+      weightsName <- paste0("weights_", yVec[y])
+      hyperparametersName <- paste0("hyperparameters_", yVec[y])
+      ensembleName <- paste0("sl_algorithm_", metalearner)
+      
+      ensembleList <- list(train_perf = train_perf, test_perf = test_perf)
+      nestedList[[s]] <- setNames(list(ensembleList, hyperparameters, weights_metamodel),
+                                   c(ensembleName, hyperparametersName, weightsName))  
+  }
+}
+}
