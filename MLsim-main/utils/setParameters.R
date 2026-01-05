ca5fcdbe0e9259f0db38d7007f15f513b7108733
Nico Kruse <nicokr1607@icloud.com>
2026-01-05 12:11:48 +0100

    temporary no nnls as it has to be added to fitSuperLearner first

--- MLsim-main/utils/setParameters.R
+++ MLsim-main/utils/setParameters.R
@@ -294,7 +294,8 @@ setParam$dgp$reliability <- c(0.7, 1)
 
 # used algorithms
 setParam$modfit$baselearner <- c("glmnet", "rpart", "ranger", "gbm")
-setParam$modfit$superlearner <- c("nnls", "glm", "glmnet", "ranger")
+setParam$modfit$superlearner <- c(#"nnls",
+  "glm","glmnet","ranger")
 
 
 
