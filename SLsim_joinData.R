564b5e5e4616b9e850a0346fc8bb4a9f6fdc3e2d
Nico Kruse <nicokr1607@icloud.com>
2026-03-16 13:38:29 +0100

    corrected joinData and first part of analysis

--- SLsim_joinData.R
+++ SLsim_joinData.R
@@ -30,239 +30,240 @@ condGrid <- expand.grid(data = dgpVec,
                         reliability = setParam$dgp$reliability
                         )  
   
-for (iDGP in dgpVec) {
-  # generate folder to save new rda files
-  depMeasures = paste0(resFolder, "/", iDGP, "/dependentMeasures")
-  if (!file.exists(depMeasures)){
-    dir.create(depMeasures)
-  }
-  
-    subGrid <- condGrid[which(condGrid$data == iDGP),]
-    performPerSample <- list()
-    
-  for(iSim in seq_len(nrow(subGrid))) {
-    sampleVec <- c(1:100)
-      
-    for (iFolder in 1:10){
-      resFilePath <- paste0("results/", iDGP, "/", iFolder, "/")
-        
-      for(iSample in 1:10){
-        sampleNumber <- sampleVec[10 * iFolder - 10 + iSample] # used to extract the files according to sample
-        resFileName <- paste0("res_", iDGP, "_N", subGrid[iSim, "N"], "_rel", subGrid[iSim,"reliability"],
-                          "_sample", sampleNumber, ".rda")
-          
-        tmp <- get(load(paste0(resFilePath,resFileName)))
-          
-        # the extracted list can now be used to save several outcomes of each list into new
-        # lists/dataframes whioch can then be saved as rda files, as well
-          
-        if(iFolder == 1 & iSample == 1) {
-          CondVec <- names(tmp[[1]])
-        }
-        
-        for (iCond in CondVec) {
-          performPerSample[[iCond]][[sampleNumber]] <- tmp[[iDGP]][[iCond]]
-        }
-      }
-    }
-    file_name <- paste0(depMeasures, "/res_", iDGP, "_N", subGrid[iSim, "N"], "_rel", 
-                        subGrid[iSim,"reliability"], "_6x9_all_iter.rda")
-    save(performPerSample, file = file_name)
-    print("done")
-    gc()
-  }
-}
+# for (iDGP in dgpVec) {
+#   # generate folder to save new rda files
+#   depMeasures = paste0(resFolder, "/", iDGP, "/dependentMeasures")
+#   if (!file.exists(depMeasures)){
+#     dir.create(depMeasures)
+#   }
+#   
+#     subGrid <- condGrid[which(condGrid$data == iDGP),]
+#     performPerSample <- list()
+#     
+#   for(iSim in seq_len(nrow(subGrid))) {
+#     sampleVec <- c(1:100)
+#       
+#     for (iFolder in 1:10){
+#       resFilePath <- paste0("results/", iDGP, "/", iFolder, "/")
+#         
+#       for(iSample in 1:10){
+#         sampleNumber <- sampleVec[10 * iFolder - 10 + iSample] # used to extract the files according to sample
+#         resFileName <- paste0("res_", iDGP, "_N", subGrid[iSim, "N"], "_rel", subGrid[iSim,"reliability"],
+#                           "_sample", sampleNumber, ".rda")
+#           
+#         tmp <- get(load(paste0(resFilePath,resFileName)))
+#           
+#         # the extracted list can now be used to save several outcomes of each list into new
+#         # lists/dataframes whioch can then be saved as rda files, as well
+#           
+#         if(iFolder == 1 & iSample == 1) {
+#           CondVec <- names(tmp[[1]])
+#         }
+#         
+#         for (iCond in CondVec) {
+#           performPerSample[[iCond]][[sampleNumber]] <- tmp[[iDGP]][[iCond]]
+#         }
+#       }
+#     }
+#     file_name <- paste0(depMeasures, "/res_", iDGP, "_N", subGrid[iSim, "N"], "_rel", 
+#                         subGrid[iSim,"reliability"], "_6x9_all_iter.rda")
+#     save(performPerSample, file = file_name)
+#     print("done")
+#     gc()
+#   }
+# }
+# 
+# # now there are 18 files, each containing all samples of all combinations within a
+# # combination of sample size, reliability and dgp.
+# 
+# # merge data to compare performances performances between dgp x N combinations
+# # (done for train and test to see potential overfit, as well)
+# 
+# for (jDGP in dgpVec) {
+#   subGrid <- condGrid[which(condGrid$data == jDGP),]
+#   filePath <- paste0(resFolder, "/", jDGP, "/dependentMeasures/")
+#   trainPerf <- list()
+#   testPerf <- list()
+#   trainPerf_SLspec <- list()
+#   testPerf_SLspec <- list()
+#   
+#   for (iFile in 1:6) {
+#     fileName <- paste0("res_", jDGP, "_N", subGrid[iFile, "N"], "_rel",
+#                        subGrid[iFile, "reliability"], "_6x9_all_iter.rda")
+#     
+#     tmp <- get(load(paste0(filePath, fileName)))
+#     CondVec <- names(tmp)
+#     
+#     for(iCond in CondVec) {
+#       metamodelVec <- setParam$modfit$superlearner
+#       
+#       for(iMetamodel in metamodelVec) {
+#         idxN <- paste0("N", subGrid[iFile, "N"])
+#         idxMetamodel <- paste0("sl_algorithm_", iMetamodel)
+#         
+#         # if (subGrid[iFile, "reliability"] == 0.7) {
+#         #   idxRel = "1"
+#         # } else {idxRel = "2"}
+#         
+#         for(iSample in 1:100) {
+#           trainPerf[[jDGP]][[idxN]] <- c(
+#             trainPerf[[jDGP]][[idxN]],
+#             tmp[[iCond]][[iSample]][[idxMetamodel]]$train_perf
+#           )
+#           testPerf[[jDGP]][[idxN]] <- c(
+#             testPerf[[jDGP]][[idxN]],
+#             tmp[[iCond]][[iSample]][[idxMetamodel]]$test_perf
+#           )
+#           
+#           trainPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]] <- c(
+#             trainPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]],
+#             tmp[[iCond]][[iSample]][[idxMetamodel]]$train_perf
+#           )
+#           testPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]] <- c(
+#             testPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]],
+#             tmp[[iCond]][[iSample]][[idxMetamodel]]$test_perf
+#           )
+#           
+#           # if needed, weights (scaled) and hyperparameters can be 
+#           # summarized this way, as well
+#           
+#         }
+#       }  
+#     }
+#   }
+#   depMeasures = paste0(resFolder, "/", jDGP, "/dependentMeasures")
+#   
+#   trainPerfFile <- paste0(depMeasures, "/res_train_perf_", jDGP, ".rda")
+#   save(trainPerf, file = trainPerfFile)
+#   
+#   testPerfFile <- paste0(depMeasures, "/res_test_perf_", jDGP, ".rda")
+#   save(testPerf, file = testPerfFile)
+#   
+#   trainPerfFile_SLspec <- paste0(depMeasures, "/res_train_perf_SLspec_", jDGP, ".rda")
+#   save(trainPerf_SLspec, file = trainPerfFile_SLspec)
+#   
+#   testPerfFile_SLspec <- paste0(depMeasures, "/res_test_perf_SLspec_", jDGP, ".rda")
+#   save(testPerf_SLspec, file = testPerfFile_SLspec)
+#   
+#   print("done")
+#   gc()
+# }
+# 
+# # Use priorly summarized lists to built data frames for each dgp and save 
+# # performances within these as columns (one for each performance metric x
+# # algorithm x N combination). These data frames can then be used to analyze 
+# # performances across all dgp x N combinations. Done for train and test 
+# # performance (table 45 x 7200 obs)
+# 
+# for (hDGP in dgpVec) {
+#   filePath <- paste0(filePath <- paste0(resFolder, "/", hDGP, "/dependentMeasures/"))
+#   perfVec <- c("train", "test")
+#   
+#   for (iPerf in perfVec) {
+#     fileName <- paste0("res_", iPerf, "_perf_", hDGP, ".rda")
+#     tmp <- get(load(paste0(filePath, fileName)))
+#     nVec <- c("N100", "N1000", "N3000")
+#     
+#     for (iN in nVec) {
+#      if (iN == "N100") {
+#        nRow <- length(tmp[[hDGP]][[iN]])/4
+#        PerfDF <- data.frame(matrix(nrow = nRow, ncol = 0))
+#      }
+#       metricVec <- c("RMSE", "MAE", "Rsquared")
+#       
+#       for (iMetric in metricVec) {
+#         algoVec <- c("glmnet", "rpart", "gbm", "ranger", "ensemble")
+#         
+#         for(iAlgo in algoVec) {
+#           if (iPerf == "train") {
+#             idxMetric <- paste0("Train", iMetric)
+#           } else if (iPerf == "test") {
+#             idxMetric = paste0("Test", iMetric)
+#           }
+#           idxValue <- which(algoVec == iAlgo)
+#           
+#           valueVec <- c()
+#           value <- NULL
+#           for (iValues in seq_len(length(tmp[[hDGP]][[iN]]))) { 
+#            if (names(tmp[[hDGP]][[iN]][iValues]) == idxMetric) {
+#               value = tmp[[hDGP]][[iN]][[iValues]][idxValue]
+#               valueVec <- c(valueVec, value)
+#            }
+#           }
+#         PerfDF[(paste0(iN, "_", iMetric, "_", iAlgo))] <- valueVec
+#         }
+#       }
+#     }
+#     depMeasures = paste0(resFolder, "/", hDGP, "/dependentMeasures")
+#     dfName <- paste0(depMeasures, "/res_", hDGP, "_df_",iPerf ,".rda")
+#     save(PerfDF, file = dfName)
+#   }
+#   print("done")
+#   gc()
+# }
+# 
+# # repeat above used loop to create data frames similar but with columns for each
+# # metalearner algorithm (table 72 x 1800 obs)
+# 
+# for (kDGP in dgpVec) {
+#   filePath <- paste0(filePath <- paste0(resFolder, "/", kDGP, "/dependentMeasures/"))
+#   perfVec <- c("train", "test")
+#   
+#   for (iPerf in perfVec) {
+#     fileName <- paste0("res_", iPerf, "_perf_SLspec_", kDGP, ".rda")
+#     tmp <- get(load(paste0(filePath, fileName)))
+#     nVec <- c("N100", "N1000", "N3000")
+#     
+#     for (iN in nVec) {
+#       slVec <- c("sl_algorithm_nnls", "sl_algorithm_glm", "sl_algorithm_glmnet", 
+#                  "sl_algorithm_ranger")
+#       
+#       for (iSL in slVec) {
+#         if (iN == "N100" & iSL == "sl_algorithm_nnls") {
+#           nRow <- length(tmp[[kDGP]][[iN]][[iSL]])/4
+#           PerfDF_SLspec <- data.frame(matrix(nrow = nRow, ncol = 0))
+#         }
+#         metricVec <- c("RMSE", "MAE", "Rsquared")
+#         
+#         for (iMetric in metricVec) {
+#           algoVec <- c("glmnet", "rpart", "gbm", "ranger", "ensemble")
+#           
+#           for(iAlgo in algoVec) {
+#             if (iPerf == "train") {
+#               idxMetric <- paste0("Train", iMetric)
+#             } else if (iPerf == "test") {
+#               idxMetric = paste0("Test", iMetric)
+#             }
+#             idxValue <- which(algoVec == iAlgo)
+#             
+#             valueVec <- c()
+#             value <- NULL
+#             for (iValues in seq_len(length(tmp[[kDGP]][[iN]][[iSL]]))) { 
+#               if (names(tmp[[kDGP]][[iN]][[iSL]][iValues]) == idxMetric) {
+#                 value = tmp[[kDGP]][[iN]][[iSL]][[iValues]][idxValue]
+#                 valueVec <- c(valueVec, value)
+#               }
+#             }
+#             if(iAlgo == "ensemble") {
+#               iAlgo <- paste0(iSL)
+#             }
+#             PerfDF_SLspec[(paste0(iN, "_", iMetric, "_", iAlgo))] <- valueVec
+#           }
+#         }
+#       }
+#     }
+#     depMeasures = paste0(resFolder, "/", kDGP, "/dependentMeasures")
+#     dfName <- paste0(depMeasures, "/res_", kDGP, "_df_SLspec_",iPerf ,".rda")
+#     save(PerfDF_SLspec, file = dfName)
+#   }
+#   print("done")
+#   gc()
+# }
 
-# now there are 18 files, each containing all samples of all combinations within a
-# combination of sample size, reliability and dgp.
-
-# merge data to compare performances performances between dgp x N combinations
-# (done for train and test to see potential overfit, as well)
-
-for (jDGP in dgpVec) {
-  subGrid <- condGrid[which(condGrid$data == jDGP),]
-  filePath <- paste0(resFolder, "/", jDGP, "/dependentMeasures/")
-  trainPerf <- list()
-  testPerf <- list()
-  trainPerf_SLspec <- list()
-  testPerf_SLspec <- list()
-  
-  for (iFile in 1:6) {
-    fileName <- paste0("res_", jDGP, "_N", subGrid[iFile, "N"], "_rel",
-                       subGrid[iFile, "reliability"], "_6x9_all_iter.rda")
-    
-    tmp <- get(load(paste0(filePath, fileName)))
-    CondVec <- names(tmp)
-    
-    for(iCond in CondVec) {
-      metamodelVec <- setParam$modfit$superlearner
-      
-      for(iMetamodel in metamodelVec) {
-        idxN <- paste0("N", subGrid[iFile, "N"])
-        idxMetamodel <- paste0("sl_algorithm_", iMetamodel)
-        
-        # if (subGrid[iFile, "reliability"] == 0.7) {
-        #   idxRel = "1"
-        # } else {idxRel = "2"}
-        
-        for(iSample in 1:100) {
-          trainPerf[[jDGP]][[idxN]] <- c(
-            trainPerf[[jDGP]][[idxN]],
-            tmp[[iCond]][[iSample]][[idxMetamodel]]$train_perf
-          )
-          testPerf[[jDGP]][[idxN]] <- c(
-            testPerf[[jDGP]][[idxN]],
-            tmp[[iCond]][[iSample]][[idxMetamodel]]$test_perf
-          )
-          
-          trainPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]] <- c(
-            trainPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]],
-            tmp[[iCond]][[iSample]][[idxMetamodel]]$train_perf
-          )
-          testPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]] <- c(
-            testPerf_SLspec[[jDGP]][[idxN]][[idxMetamodel]],
-            tmp[[iCond]][[iSample]][[idxMetamodel]]$test_perf
-          )
-          
-          # if needed, weights (scaled) and hyperparameters can be 
-          # summarized this way, as well
-          
-        }
-      }  
-    }
-  }
-  depMeasures = paste0(resFolder, "/", jDGP, "/dependentMeasures")
-  
-  trainPerfFile <- paste0(depMeasures, "/res_train_perf_", jDGP, ".rda")
-  save(trainPerf, file = trainPerfFile)
-  
-  testPerfFile <- paste0(depMeasures, "/res_test_perf_", jDGP, ".rda")
-  save(testPerf, file = testPerfFile)
-  
-  trainPerfFile_SLspec <- paste0(depMeasures, "/res_train_perf_SLspec_", jDGP, ".rda")
-  save(trainPerf_SLspec, file = trainPerfFile_SLspec)
-  
-  testPerfFile_SLspec <- paste0(depMeasures, "/res_test_perf_SLspec_", jDGP, ".rda")
-  save(testPerf_SLspec, file = testPerfFile_SLspec)
-  
-  print("done")
-  gc()
-}
-
-# Use priorly summarized lists to built data frames for each dgp and save 
-# performances within these as columns (one for each performance metric x
-# algorithm x N combination). These data frames can then be used to analyze 
-# performances across all dgp x N combinations. Done for train and test 
-# performance (table 45 x 7200 obs)
-
-for (hDGP in dgpVec) {
-  filePath <- paste0(filePath <- paste0(resFolder, "/", hDGP, "/dependentMeasures/"))
-  perfVec <- c("train", "test")
-  
-  for (iPerf in perfVec) {
-    fileName <- paste0("res_", iPerf, "_perf_", hDGP, ".rda")
-    tmp <- get(load(paste0(filePath, fileName)))
-    nVec <- c("N100", "N1000", "N3000")
-    
-    for (iN in nVec) {
-     if (iN == "N100") {
-       nRow <- length(tmp[[hDGP]][[iN]])/4
-       PerfDF <- data.frame(matrix(nrow = nRow, ncol = 0))
-     }
-      metricVec <- c("RMSE", "MAE", "Rsquared")
-      
-      for (iMetric in metricVec) {
-        algoVec <- c("glmnet", "rpart", "gbm", "ranger", "ensemble")
-        
-        for(iAlgo in algoVec) {
-          if (iPerf == "train") {
-            idxMetric <- paste0("Train", iMetric)
-          } else if (iPerf == "test") {
-            idxMetric = paste0("Test", iMetric)
-          }
-          idxValue <- which(algoVec == iAlgo)
-          
-          valueVec <- c()
-          value <- NULL
-          for (iValues in seq_len(length(tmp[[hDGP]][[iN]]))) { 
-           if (names(tmp[[hDGP]][[iN]][iValues]) == idxMetric) {
-              value = tmp[[hDGP]][[iN]][[iValues]][idxValue]
-              valueVec <- c(valueVec, value)
-           }
-          }
-        PerfDF[(paste0(iN, "_", iMetric, "_", iAlgo))] <- valueVec
-        }
-      }
-    }
-    depMeasures = paste0(resFolder, "/", hDGP, "/dependentMeasures")
-    dfName <- paste0(depMeasures, "/res_", hDGP, "_df_",iPerf ,".rda")
-    save(PerfDF, file = dfName)
-  }
-  print("done")
-  gc()
-}
-
-# repeat above used loop to create data frames similar but with columns for each
-# metalearner algorithm (table 72 x 1800 obs)
-
-for (kDGP in dgpVec) {
-  filePath <- paste0(filePath <- paste0(resFolder, "/", kDGP, "/dependentMeasures/"))
-  perfVec <- c("train", "test")
-  
-  for (iPerf in perfVec) {
-    fileName <- paste0("res_", iPerf, "_perf_SLspec_", kDGP, ".rda")
-    tmp <- get(load(paste0(filePath, fileName)))
-    nVec <- c("N100", "N1000", "N3000")
-    
-    for (iN in nVec) {
-      slVec <- c("sl_algorithm_nnls", "sl_algorithm_glm", "sl_algorithm_glmnet", 
-                 "sl_algorithm_ranger")
-      
-      for (iSL in slVec) {
-        if (iN == "N100" & iSL == "sl_algorithm_nnls") {
-          nRow <- length(tmp[[kDGP]][[iN]][[iSL]])/4
-          PerfDF_SLspec <- data.frame(matrix(nrow = nRow, ncol = 0))
-        }
-        metricVec <- c("RMSE", "MAE", "Rsquared")
-        
-        for (iMetric in metricVec) {
-          algoVec <- c("glmnet", "rpart", "gbm", "ranger", "ensemble")
-          
-          for(iAlgo in algoVec) {
-            if (iPerf == "train") {
-              idxMetric <- paste0("Train", iMetric)
-            } else if (iPerf == "test") {
-              idxMetric = paste0("Test", iMetric)
-            }
-            idxValue <- which(algoVec == iAlgo)
-            
-            valueVec <- c()
-            value <- NULL
-            for (iValues in seq_len(length(tmp[[kDGP]][[iN]][[iSL]]))) { 
-              if (names(tmp[[kDGP]][[iN]][[iSL]][iValues]) == idxMetric) {
-                value = tmp[[kDGP]][[iN]][[iSL]][[iValues]][idxValue]
-                valueVec <- c(valueVec, value)
-              }
-            }
-            if(iAlgo == "ensemble") {
-              iAlgo <- paste0(iSL)
-            }
-            PerfDF_SLspec[(paste0(iN, "_", iMetric, "_", iAlgo))] <- valueVec
-          }
-        }
-      }
-    }
-    depMeasures = paste0(resFolder, "/", kDGP, "/dependentMeasures")
-    dfName <- paste0(depMeasures, "/res_", kDGP, "_df_SLspec_",iPerf ,".rda")
-    save(PerfDF_SLspec, file = dfName)
-  }
-  print("done")
-  gc()
-}
-
-# for one research question one has to look at all factors that where manipulated
-# thus a data frame with 1296 columns and 100 rows is needed (all metrics x all 
+# data frame with 1296 columns and 100 rows  (all metrices x all 
 # conditions)
+
+# this data frame is needed for all analyses
 
 for (lDGP in dgpVec) {
   subGrid <- condGrid[which(condGrid$data == lDGP),]
--- SLsim_resultsAnalysis.R
+++ SLsim_resultsAnalysis.R
@@ -2,8 +2,246 @@
 #___________________________________Analysis__________________________________#
 ###############################################################################
 
+# load packages and parameters
 
-# Forschungsfragen anschauen -> AV(s)? 
+library(afex) # for aov_ez()
+library(effectsize) # for efect size calculation; generalized eta²
+library(emmeans)
+library(ggplot2)
+library(tidyverse)
+library(cowplot)
+library(purrr)
+
+source("MLsim-main/utils/setParameters.R")
+
+# load and restructure data to match expected data frame for anova 
+dgpVec <- c("inter", "pwlinear", "nonlinear3")
+
+for (iDGP in dgpVec) {
+  dataVec = c("train", "test")
+  for(iData in dataVec) {
+    
+    filePath <- paste0("results/", iDGP, "/dependentMeasures/")
+    fileName <- paste0("res_", iDGP, "_df_allCond_", iData, ".rda")
+    
+    tmp <- get(load(paste0(filePath, fileName)))
+    if(iData == "train") {
+      tmp_long <- pivot_longer(tmp,
+                          cols = everything(),      
+                          names_to = "ID",
+                          values_to = "train_Value")
+      
+      tmp_long <- separate(tmp_long, ID, into = c("rel", "R2_tmp", "lin_inter_tmp",
+                                                  "lin_inter_tmp2", "N", "Metric", 
+                                                  "Model_tmp", "ens1", "ens2"), 
+                           sep = "_") %>%
+        mutate(R2 = as.factor(sub("R2([0-9.]+)lin", "\\1", R2_tmp))) %>%
+        mutate(Model = case_when(Model_tmp == "sl" ~ paste(Model_tmp, ens1, ens2, 
+                                                           sep = "_"),
+                                 Model_tmp != "sl" ~ Model_tmp)) %>%
+        mutate(lin_inter = as.factor(paste("lin", lin_inter_tmp, lin_inter_tmp2, 
+                                           sep = "_")))
+      
+      tempCols <- c("R2_tmp", "lin_inter_tmp", "lin_inter_tmp2", "Model_tmp", 
+                    "ens1", "ens2")
+      tmp_long <- tmp_long[, !names(tmp_long) %in% tempCols]
+      tmp_long$dgp <- iDGP
+      tmp_long <- tmp_long[, order(colnames(tmp_long))]
+      
+      rmse <- subset(tmp_long, Metric == "RMSE") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE)) %>%
+        rename(!!paste0(iData, "_rmse") := !!sym(paste0(iData, "_Value")))
+      rmse$Metric <- NULL
+      rmse$sample_no <- NULL
+      
+      mae  <- subset(tmp_long, tmp_long$Metric == "MAE") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE))%>%
+        rename(!!paste0(iData, "_mae") := !!sym(paste0(iData, "_Value")))
+      mae$Metric <- NULL
+      mae$sample_no <- NULL
+      
+      rsq  <- subset(tmp_long, tmp_long$Metric == "Rsquared") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE)) %>%
+        rename(!!paste0(iData, "_rsquared") := !!sym(paste0(iData, "_Value")))
+      rsq$Metric <- NULL
+      rsq$sample_no <- NULL
+      
+      tmp_final = left_join(rmse, mae, by = c("dgp", "lin_inter", "Model", "N", "R2", "rel", "ID"))
+      tmp_final = left_join(tmp_final, rsq, by = c("dgp", "lin_inter", "Model", "N", "R2", "rel", "ID"))
+      tmp_final <- tmp_final[, order(colnames(tmp_final))]
+      
+    } else if (iData == "test") {
+      tmp_long <- pivot_longer(tmp,
+                               cols = everything(),      
+                               names_to = "ID",
+                               values_to = "test_Value")
+      
+      tmp_long <- separate(tmp_long, ID, into = c("rel", "R2_tmp", "lin_inter_tmp",
+                                                  "lin_inter_tmp2", "N", "Metric", 
+                                                  "Model_tmp", "ens1", "ens2"), 
+                           sep = "_") %>%
+        mutate(R2 = as.factor(sub("R2([0-9.]+)lin", "\\1", R2_tmp))) %>%
+        mutate(Model = case_when(Model_tmp == "sl" ~ paste(Model_tmp, ens1, ens2, 
+                                                           sep = "_"),
+                                 Model_tmp != "sl" ~ Model_tmp)) %>%
+        mutate(lin_inter = as.factor(paste("lin", lin_inter_tmp, lin_inter_tmp2, 
+                                           sep = "_")))
+      
+      tempCols <- c("R2_tmp", "lin_inter_tmp", "lin_inter_tmp2", "Model_tmp", 
+                    "ens1", "ens2")
+      tmp_long <- tmp_long[, !names(tmp_long) %in% tempCols]
+      tmp_long$dgp <- iDGP
+      tmp_long <- tmp_long[, order(colnames(tmp_long))]
+      
+      rmse <- subset(tmp_long, Metric == "RMSE") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE)) %>%
+        rename(!!paste0(iData, "_rmse") := !!sym(paste0(iData, "_Value")))
+      rmse$Metric <- NULL
+      rmse$sample_no <- NULL
+      
+      mae  <- subset(tmp_long, tmp_long$Metric == "MAE") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE))%>%
+        rename(!!paste0(iData, "_mae") := !!sym(paste0(iData, "_Value")))
+      mae$Metric <- NULL
+      mae$sample_no <- NULL
+      
+      rsq  <- subset(tmp_long, tmp_long$Metric == "Rsquared") %>%
+        group_by(dgp, lin_inter, Model, N, R2, rel) %>%
+        mutate(sample_no = seq_len(n())) %>%
+        ungroup() %>%
+        mutate(ID = interaction(dgp, lin_inter, Model, N, R2, rel, sample_no, drop = TRUE)) %>%
+        rename(!!paste0(iData, "_rsquared") := !!sym(paste0(iData, "_Value")))
+      rsq$Metric <- NULL
+      rsq$sample_no <- NULL
+      
+      tmp_final = left_join(rmse, mae, by = c("dgp", "lin_inter", "Model", "N", "R2", "rel", "ID"))
+      tmp_final = left_join(tmp_final, rsq, by = c("dgp", "lin_inter", "Model", "N", "R2", "rel", "ID"))
+      tmp_final <- tmp_final[, order(colnames(tmp_final))]
+    }
+ 
+    if (iData == "train") {
+    train <- tmp_final
+    } else if (iData == "test") {
+      test <- tmp_final
+      aov_dgpSpec <- left_join(train, test,
+                               by = c("dgp", "lin_inter", "Model", "N", "R2", "rel", "ID"),
+                               suffix = c("", ""))
+    }
+  }
+  
+  if (iDGP == "inter") {
+    aov_data <- aov_dgpSpec
+  } else {
+    aov_data <- rbind(aov_data, aov_dgpSpec)
+  }
+}
+
+
+
+
+
+
+
+
+# aov_data for anova w/ 
+# ... between: 3 x 2 x 3 x 3 x 3  
+#   N         (3)   {100, 1000, 3000}
+#   rel       (2)   {0.7, 1}
+#   R2        (3)   {0.2, 0.5, 0.8}
+#   lin_inter (3)   {0.0_1.0, 0.5_0.5, 1.0_0.0}
+#   dgp       (3)   {inter, pwlinear, nonlinear3}
+# ... within:
+#   model {glmnet, rpart, gbm, ranger}
+
+
+
+
+   
+rmse <- subset(aov_data, aov_data$Metric == "RMSE") %>%
+    group_by(N, dgp, Model) %>%
+    mutate(sim = row_number()) %>%
+    ungroup() %>%
+    mutate(ID = interaction(N, dgp, sim, drop = TRUE)) 
+
+mae  <- subset(aov_data, aov_data$Metric == "MAE") %>%
+  group_by(N, dgp, Model) %>%
+  mutate(sim = row_number()) %>%
+  ungroup() %>%
+  mutate(ID = interaction(N, dgp, sim, drop = TRUE))
+
+rsq  <- subset(aov_data, aov_data$Metric == "Rsquared") %>%
+  group_by(N, dgp, Model) %>%
+  mutate(sim = row_number()) %>%
+  ungroup() %>%
+  mutate(ID = interaction(N, dgp, sim, drop = TRUE))
+ 
+   
+aov_rmse <- aov_ez(id = "ID",
+                   dv = "Value",
+                   data = rmse,
+                   between = c("N", "dgp"),
+                   within = "Model",
+                   fun_aggregate = mean)
+
+aov_mae <- aov_ez(id = "ID",
+                  dv = "Value",
+                  data = mae,
+                  between = c("N", "dgp"),
+                  within = "Model",
+                  fun_aggregate = mean)
+
+aov_rsquared <- aov_ez(id = "ID",
+                       dv = "Value",
+                       data = rsq,
+                       between = c("N", "dgp"),
+                       within = "Model",
+                       fun_aggregate = mean)
+
+
+
+
+
+
+ 
+  
+  
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # anova mit sample faktoren -> delta Performanz Super learner vs. bestes Einzelmodell
