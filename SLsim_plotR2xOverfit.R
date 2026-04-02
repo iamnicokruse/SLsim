3133516957e8218ff4519dec4d5a89ea7a531197
Nico Kruse <nicokr1607@icloud.com>
2026-04-02 17:41:24 +0200

    more plots

--- SLsim_plotR2xOverfit.R
+++ SLsim_plotR2xOverfit.R
@@ -32,8 +32,6 @@ performanceStats <- plot_data %>%
     .groups = "drop"
   )
 
-## plot results for paper (subset of the data)
-
 # sl_algorithm_nnls, sl_algorithm_glm, sl_algorithm_glmnet, sl_algorithm_ranger, 
 # glmnet, rpart, gbm, ranger
 modelLineColors <- c("#1F77B4", "#FF7F0E", "#2CA02C" ,"#D62728" ,"#9467BD",
@@ -43,11 +41,12 @@ modelPointShapes <- c(18, 1, 3, 4, 15, 16, 17, 8)
 # lin_inter_0.0_1.0, lin_inter_0.5_0.5, lin_inter_1.0_0.0
 lin_interPanelColors <- c("#F44336", "#9C27B0", "#1565C0")
 relColValues <- c("#999999", "#444444") # rel = 0.7, rel = 1
+R2ColValues <- c("#999999", "#555555", "#222222")
 linetypeVec <- c("dotted", "dashed", "solid")
 
-
-plotR2 <- function(data, plotMeasure, title = "", yLabel = "",
-                        yMin = NULL, yMax = NULL){
+# plot R2 similar to MLsim
+plotR2_likeMLsim <- function(data, plotMeasure, title = "", yLabel = "",
+                             yMin = NULL, yMax = NULL){
   ggplot(data,
          aes(x = N, y = plotMeasure, 
              group = interaction(R2, Model), linetype = R2, color = Model)) +
@@ -84,7 +83,7 @@ themeFunction <- function(plotObject, guides = T){
     panel.grid.major = element_line(linewidth = 0.15, linetype = 'solid', color = "lightgrey"), 
     panel.grid.minor = element_line(linewidth = 0.1, linetype = 'solid', color = "lightgrey"),
     panel.background = element_rect(color = "white", fill = "white"),
-    plot.title = element_text(size = 30, face = "bold"),
+    plot.title = element_text(size = 15, face = "bold"),
     axis.text.y = element_text(size = 20),
     axis.text.x = element_text(size = 20),
     axis.title.x = element_text(size = 20),
@@ -114,7 +113,7 @@ subFig_nl3$Model <- gsub("algorithm_", "", subFig_nl3$Model)
 
 
 # create plots
-fig_inter <- plotR2(subFig_inter, plotMeasure = subFig_inter$M, title = "", yLabel = expression(R[test]^2),
+fig_inter <- plotR2_likeMLsim(subFig_inter, plotMeasure = subFig_inter$M, title = "", yLabel = expression(R[test]^2),
                     yMin = 0, yMax = 0.9)
 (fig_inter <- themeFunction(fig_inter))
 (fig_inter <- fig_inter + geom_text(
@@ -122,7 +121,7 @@ fig_inter <- plotR2(subFig_inter, plotMeasure = subFig_inter$M, title = "", yLab
                             subFig_inter$R2 == "0.8"),],
   aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
 
-fig_pw <- plotR2(subFig_pw, plotMeasure = subFig_pw$M, title = "", yLabel = expression(R[test]^2),
+fig_pw <- plotR2_likeMLsim(subFig_pw, plotMeasure = subFig_pw$M, title = "", yLabel = expression(R[test]^2),
                     yMin = 0, yMax = 0.9)
 (fig_pw <- themeFunction(fig_pw))
 (fig_pw <- fig_pw + geom_text(
@@ -130,7 +129,7 @@ fig_pw <- plotR2(subFig_pw, plotMeasure = subFig_pw$M, title = "", yLabel = expr
                               subFig_pw$R2 == "0.8"),],
   aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
 
-fig_nl3 <- plotR2(subFig_nl3, plotMeasure = subFig_nl3$M, title = "", yLabel = expression(R[test]^2),
+fig_nl3 <- plotR2_likeMLsim(subFig_nl3, plotMeasure = subFig_nl3$M, title = "", yLabel = expression(R[test]^2),
                     yMin = 0, yMax = 0.9)
 (fig_nl3 <- themeFunction(fig_nl3))
 (fig_nl3 <- fig_nl3 + geom_text(
@@ -157,3 +156,186 @@ ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_nonlinear3.png")
                 height = 13.00,
                 units = "in")
 
+# similar plots but with only meta model "sl_algorithm_glmnet" and subset of 
+# data (dgp = "inter")
+performanceStats_oneSL <- subset(performanceStats, Model %in% c("gbm", 
+                                                                "glmnet", 
+                                                                "rpart",
+                                                                "ranger", 
+                                                                "sl_algorithm_glmnet"
+                                                                ))
+# dgp = inter
+subFig_inter_oneSL <- subset(performanceStats_oneSL, dgp == "inter")
+subFig_inter_oneSL$Model <- gsub("algorithm_", "", subFig_inter_oneSL$Model)
+
+fig_inter_oneSL <- plotR2_likeMLsim(subFig_inter_oneSL,
+                                    plotMeasure = subFig_inter_oneSL$M, 
+                                    title = "dgp = inter", yLabel = expression(R[test]^2),
+                                    yMin = 0, yMax = 0.9)
+(fig_inter_oneSL <- themeFunction(fig_inter_oneSL))
+(fig_inter_oneSL <- fig_inter_oneSL + geom_text(
+  data = subFig_inter_oneSL[which(subFig_inter_oneSL$N == "N1000" &
+                              subFig_inter_oneSL$R2 == "0.8"),],
+  aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
+
+# dgp = pwlinear
+subFig_pw_oneSL <- subset(performanceStats_oneSL, dgp == "pwlinear")
+subFig_pw_oneSL$Model <- gsub("algorithm_", "", subFig_pw_oneSL$Model)
+
+fig_pw_oneSL <- plotR2_likeMLsim(subFig_pw_oneSL,
+                                    plotMeasure = subFig_pw_oneSL$M, 
+                                    title = "dgp = pw", yLabel = expression(R[test]^2),
+                                    yMin = 0, yMax = 0.9)
+(fig_pw_oneSL <- themeFunction(fig_pw_oneSL))
+(fig_pw_oneSL <- fig_pw_oneSL + geom_text(
+  data = subFig_pw_oneSL[which(subFig_pw_oneSL$N == "N1000" &
+                                    subFig_pw_oneSL$R2 == "0.8"),],
+  aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
+
+# dgp = nonlinear3
+subFig_nl3_oneSL <- subset(performanceStats_oneSL, dgp == "nonlinear3")
+subFig_nl3_oneSL$Model <- gsub("algorithm_", "", subFig_nl3_oneSL$Model)
+
+fig_nl3_oneSL <- plotR2_likeMLsim(subFig_nl3_oneSL,
+                                    plotMeasure = subFig_nl3_oneSL$M, 
+                                    title = "dgp = nl3", yLabel = expression(R[test]^2),
+                                    yMin = 0, yMax = 0.9)
+(fig_nl3_oneSL <- themeFunction(fig_nl3_oneSL))
+(fig_nl3_oneSL <- fig_nl3_oneSL + geom_text(
+  data = subFig_nl3_oneSL[which(subFig_nl3_oneSL$N == "N1000" &
+                                    subFig_nl3_oneSL$R2 == "0.8"),],
+  aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
+
+ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_inter_oneSL.png"),
+                plot = fig_inter_oneSL,
+                width = 18.30,
+                height = 13.00,
+                units = "in")
+
+ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_pw_oneSL.png"),
+                plot = fig_pw_oneSL,
+                width = 18.30,
+                height = 13.00,
+                units = "in")
+
+ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_nl3_oneSL.png"),
+                plot = fig_nl3_oneSL,
+                width = 18.30,
+                height = 13.00,
+                units = "in")
+
+
+# similar plots but with only meta models and subset of 
+# data (dgp = "inter")
+# plotR2_likeMLsim_onlySL <- function(data, plotMeasure, title = "", yLabel = "",
+#                              yMin = NULL, yMax = NULL){
+#   ggplot(data,
+#          aes(x = N, y = plotMeasure, 
+#              group = interaction(R2, Model), linetype = R2, color = Model)) +
+#     geom_line(linewidth = 1, alpha = 0.4) +
+#     geom_point(aes(shape = Model), size = 3) +
+#     scale_linetype_manual(name = expression(R[sim]^2), values = linetypeVec,
+#                           guide = guide_legend(override.aes = list(size = 2, alpha = 0.8))) +
+#     scale_color_manual(values = modelLineColors[5:8]) +
+#     scale_shape_manual(values = modelPointShapes[5:8]) +
+#     scale_y_continuous(limits = c(yMin, yMax), breaks=seq(
+#       ifelse(yMin %% 0.2 == 0, yMin, yMin + 0.1), yMax, 0.2)) +
+#     geom_hline(aes(yintercept = 0)) +
+#     facet_grid2(rel ~ lin_inter,
+#                 strip = strip_themed(
+#                   background_x = list(element_rect(fill = alpha(lin_interPanelColors[1], 0.4)),
+#                                       element_rect(fill = alpha(lin_interPanelColors[2], 0.4)),
+#                                       element_rect(fill = alpha(lin_interPanelColors[3], 0.4))),
+#                   background_y = list(element_rect(fill = alpha(relColValues[2], 0.4)),
+#                                       element_rect(fill = alpha(relColValues[1], 0.4))))) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[1], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[1]) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[2], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[2]) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[3], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[3]) +
+#     ylab(yLabel) +
+#     xlab("Sample Size (N)") +
+#     ggtitle(title) +
+#     guides(colour = "none", shape = "none")
+# }
+# 
+# performanceStats_onlySL <- subset(performanceStats, Model %in% c("sl_algorithm_glm",
+#                                                                  "sl_algorithm_nnls",
+#                                                                  "sl_algorithm_ranger",
+#                                                                  "sl_algorithm_glmnet"
+#                                                                  ))
+# 
+# subFig_inter_onlySL <- subset(performanceStats_onlySL, dgp == "inter")
+# subFig_inter_onlySL$Model <- gsub("algorithm_", "", subFig_inter_onlySL$Model)
+# 
+# fig_inter_onlySL <- plotR2_likeMLsim_onlySL(subFig_inter_onlySL,
+#                                             plotMeasure = subFig_inter_onlySL$M, 
+#                                             title = "dgp = inter", yLabel = expression(R[test]^2),
+#                                             yMin = 0, yMax = 0.9)
+# (fig_inter_onlySL <- themeFunction(fig_inter_onlySL))
+# (fig_inter_onlySL <- fig_inter_onlySL + geom_text(
+#   data = subFig_inter_onlySL[which(subFig_inter_onlySL$N == "N1000" &
+#                                     subFig_inter_onlySL$R2 == "0.8"),],
+#   aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(3)))
+# 
+# ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_inter_onlySL.png"),
+#                 plot = fig_inter_onlySL,
+#                 width = 18.30,
+#                 height = 13.00,
+#                 units = "in")
+
+# plot R2 with R2sim Levels in rows for subset of data (rel = 1, dgp = "inter")
+
+# new function to structure plot accordingly
+# plotR2_rowR2 <- function(data, plotMeasure, title = "", yLabel = "",
+#                              yMin = NULL, yMax = NULL){
+#   ggplot(data,
+#          aes(x = N, y = plotMeasure, 
+#              group = Model, color = Model)) +
+#     geom_line(linewidth = 1, alpha = 0.4) +
+#     geom_point(aes(shape = Model), size = 3) +
+#     scale_color_manual(values = modelLineColors) +
+#     scale_shape_manual(values = modelPointShapes) +
+#     scale_y_continuous(limits = c(yMin, yMax), breaks=seq(
+#       ifelse(yMin %% 0.2 == 0, yMin, yMin + 0.1), yMax, 0.2)) +
+#     geom_hline(aes(yintercept = 0)) +
+#     facet_grid2(R2 ~ lin_inter,
+#                 strip = strip_themed(
+#                   background_x = list(element_rect(fill = alpha(lin_interPanelColors[1], 0.4)),
+#                                       element_rect(fill = alpha(lin_interPanelColors[2], 0.4)),
+#                                       element_rect(fill = alpha(lin_interPanelColors[3], 0.4))),
+#                   background_y = list(element_rect(fill = alpha(R2ColValues[3], 0.4)),
+#                                       element_rect(fill = alpha(R2ColValues[2], 0.4)),
+#                                       element_rect(fill = alpha(R2ColValues[1], 0.4))))) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[1], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[1]) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[2], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[2]) +
+#     geom_hline(yintercept = setParam$dgp$Rsquared[3], col = "black",
+#                alpha = 0.4, linetype = linetypeVec[3]) +
+#     ylab(yLabel) +
+#     xlab("Sample Size (N)") +
+#     ggtitle(title) +
+#     guides(colour = "none", shape = "none")
+# }
+# 
+# performanceStats_rowR2 <- subset(performanceStats, rel == "1")
+# 
+# subFig_inter_rowR2 <- subset(performanceStats_rowR2, dgp == "inter")
+# subFig_inter_rowR2$Model <- gsub("algorithm_", "", subFig_inter_rowR2$Model)
+# 
+# fig_inter_rowR2 <- plotR2_rowR2(subFig_inter_rowR2,
+#                                     plotMeasure = subFig_inter_rowR2$M, 
+#                                     title = "dgp = inter", yLabel = expression(R[test]^2),
+#                                     yMin = 0, yMax = 0.9)
+# (fig_inter_rowR2 <- themeFunction(fig_inter_rowR2))
+# (fig_inter_rowR2 <- fig_inter_rowR2 + geom_text(
+#   data = subFig_inter_rowR2[which(subFig_inter_rowR2$N == "N1000"),],
+#   aes(x = N, y = 0.85, label = Model, color = Model), position = position_dodge(4)))
+# 
+# ggplot2::ggsave(filename = paste0(plotFolder, "/performancePlot_inter_rowR2_rel1.png"),
+#                 plot = fig_inter_rowR2,
+#                 width = 18.30,
+#                 height = 13.00,
+#                 units = "in")
