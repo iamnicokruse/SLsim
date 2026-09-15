009d693a1467e3098b54740906374951daab0706
Nico Kruse <nicokr1607@icloud.com>
2026-09-15 18:53:38 +0200

    new testList with N = 10,000

---
+++ SLsim_testList_development.R
@@ -0,0 +1,76 @@
+###############################################################################
+#____________________________TestList_development_____________________________#
+###############################################################################
+# packages
+library(tidyverse)
+library(mvtnorm)
+library(truncnorm)
+
+
+# parameters
+source("MLsim-main/utils/setParameters.R")
+
+# simulation functions
+source("MLsim-main/utils/sampleInteractionData.R")
+source("MLsim-main/utils/sampleNonlinearData.R")
+source("MLsim-main/utils/samplePiecewiseLinearData.R")
+source("MLsim-main/utils/simTools.R")
+
+# generate grid with all combinations of simulation conditions
+gridInter <- expand.grid(N = setParam$dgp$N,
+                         reliability = setParam$dgp$reliability)
+
+# add seeds to grid
+set.seed(123)
+seedNum <- sample(1:999999, dim(gridInter)[1], replace = FALSE) 
+gridInter$sampleSeed <- seedNum[1:dim(gridInter)[1]]
+
+# add dgp type column to grid
+gridNL <- cbind(data = "inter", gridInter)  
+
+# add other dgps and seeds
+set.seed(321)
+seedNum <- sample(1:999999, dim(gridInter)[1], replace = FALSE) 
+
+gridFull <- rbind(gridNL, 
+                  cbind(data = "pwlinear", 
+                        gridInter[,!colnames(gridInter) %in% "sampleSeed"], 
+                        sampleSeed = seedNum))
+
+# add nonlinear dgp with 3 dummy variables
+set.seed(213)
+seedNum <- sample(1:999999, dim(gridInter)[1], replace = FALSE) 
+
+gridFull <- rbind(gridFull, 
+                  cbind(data = "nonlinear3", 
+                        gridInter[,!colnames(gridInter) %in% "sampleSeed"], 
+                        sampleSeed = seedNum)) 
+
+# check uniqueness of set seeds
+length(unique(gridFull$sampleSeed))
+
+# sample data
+createData <- function(data, N, reliability){
+  
+  if (data == "inter"){
+    environment(sampleInteractionData) <- environment()  
+  } else if (data == "nonlinear3") {
+    environment(sampleNonlinearData) <- environment()  
+  } else if (data == "pwlinear") {
+    environment(samplePiecewiseLinearData) <- environment()  
+  } else {
+    stop("We can only simulate inter, nonlinear or piecewise linear data!")
+  }
+  
+  if (data == "inter"){
+    sampleInteractionData() # run function to actually create data set
+  } else if (data == "nonlinear3") {
+    sampleNonlinearData()
+  } else if (data == "pwlinear") {
+    samplePiecewiseLinearData()
+  }
+}
+
+pTrash <- 25
+testList <- do.call(mapply, c(FUN = createData, gridFull[,1:3]))
+save(testList, file = "testList.rda")
